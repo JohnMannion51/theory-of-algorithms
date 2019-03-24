64d182529475d5bf5e698235c43e164bea26ec28
johnmannion51 <theminnion@theory-of-algorithms.europe-west2-c.c.black-pier-165413.internal>
2019-03-24 19:23:59 +0000

    adding padding for sha256

--- padfile.c
+++ padfile.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
 union msgblock {
   uint8_t  e[64];
   uint32_t t[16];
@@ -12,21 +11,32 @@ union msgblock {
 int main(int argc, char *argv[]){
 
   union msgblock M;
-  
+  uint64_t nobits = 0;
   uint64_t nobytes;
- 
-  FILE* f;
 
+  FILE* f;
   f = fopen(argv[1], "r");
 
   while (!feof(f)) {
-
     nobytes = fread(M.e, 1, 64, f);
-    printf("%llu\n", nobytes);
- 
+    nobits = nobits + (nobytes * 8);
+    if (nobytes < 56){
+      printf("I've found a block with less than 55 bytes !\n");
+      M.e[nobytes] = 0x80;
+      while (nobytes < 56) {
+        nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00; 
+      }
+      M.s[7] = nobits; 
+    }
   }
 
   fclose(f);
+
+  for (int i = 0; i < 64; i++){
+    printf("%x ",M.e[i]);
+   }
+   printf("\n");
 
   return 0 ;
 }
