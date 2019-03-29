74a54ad752916923dd948ba9abeea596736fe8ca
johnmannion51 <theminnion@theory-of-algorithms.europe-west2-c.c.black-pier-165413.internal>
2019-03-29 15:44:12 +0000

    created back up of sha256.c named in hash.c

--- hash.c
+++ hash.c
@@ -26,7 +26,6 @@ uint32_t SIG1(uint32_t x);
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
 uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
 
-int nextmsgblock();
 
 int main(int argc, char *argv[]){
   
